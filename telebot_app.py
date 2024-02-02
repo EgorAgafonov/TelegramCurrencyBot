da2653c7f9d5b064f539112d8da5d01ccec3df9a
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-02-02 18:10:03 +0300

    Files updated

--- telebot_app.py
+++ telebot_app.py
@@ -3,3 +3,31 @@ from settings import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+
+@bot.message_handler(content_types=["voice"])
+def voice_message_answer(message: telebot.types.Message):
+    bot.send_message(message.chat.id, "Как прекрасно ты звучишь ;-)!")
+
+
+@bot.message_handler(content_types=["text"])
+def text_message_answer(message: telebot.types.Message):
+    bot.send_message(message.chat.id, f"Привет, {message.chat.username}!")
+
+
+@bot.message_handler(content_types=["photo"])
+def foto_message_answer(message: telebot.types.Message):
+    bot.send_message(message.chat.id, "Замечательное фото!")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message: telebot.types.Message):
+    pass
+
+
+# Обрабатываются все документы и аудиозаписи
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_docs_audio(message):
+    pass
+
+
+bot.polling(none_stop=True)
