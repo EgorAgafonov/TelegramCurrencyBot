488e442ce6abc5e92e6eef8fbdbe55489460357a
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-02-02 18:46:54 +0300

    Files updated

--- telebot_app.py
+++ telebot_app.py
@@ -4,30 +4,27 @@ from settings import TOKEN
 bot = telebot.TeleBot(TOKEN)
 
 
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message: telebot.types.Message):
+    bot.send_message(message.chat.id, "РАЗРАБОТЧИКУ: Заполнить документацию для команды /help, /start!!!")
+
+
 @bot.message_handler(content_types=["voice"])
 def voice_message_answer(message: telebot.types.Message):
-    bot.send_message(message.chat.id, "Как прекрасно ты звучишь ;-)!")
+    bot.reply_to(message, "Как прекрасно ты звучишь ;-)!")
 
 
 @bot.message_handler(content_types=["text"])
 def text_message_answer(message: telebot.types.Message):
-    bot.send_message(message.chat.id, f"Привет, {message.chat.username}!")
+    print(message.text.split(' '))
+    bot.reply_to(message, f"Привет, {message.chat.username}!")
 
 
 @bot.message_handler(content_types=["photo"])
 def foto_message_answer(message: telebot.types.Message):
-    bot.send_message(message.chat.id, "Замечательное фото!")
+    bot.reply_to(message, "Nice meme XDD")
 
 
-@bot.message_handler(commands=['start', 'help'])
-def handle_start_help(message: telebot.types.Message):
-    pass
-
-
-# Обрабатываются все документы и аудиозаписи
-@bot.message_handler(content_types=['document', 'audio'])
-def handle_docs_audio(message):
-    pass
 
 
 bot.polling(none_stop=True)
