8db7852b6125d091abca50d1b6820ad349e4dde1
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-02-07 01:02:13 +0300

    Files updated

--- bot_exceptions_class.py
+++ bot_exceptions_class.py
@@ -16,6 +16,11 @@ class CryptoConverter:
 
         status, result = currency_API.conversion_of_currency_pair(api_key, amount=quantity, base_code=base_code,
                                                                   target_code=target_code)
+        try:
+            float(quantity)
+        except ValueError:
+            raise ConvertionException(f"Ошибка!\n"
+                                      f"Указанное значение: '{quantity}' не является числом.")
 
         if base_code == target_code:
             raise ConvertionException(f"Ошибка!\nУказаны две одинаковые валюты или числовое значение вместо "
@@ -23,23 +28,21 @@ class CryptoConverter:
                                       f"Логика вышла из чата😜.\n "
                                       f"Вот корректный пример ввода: '100 USD RUB'")
         try:
-            keys[base_code]
-            base_code.isalpha()
+            check_1 = keys[base_code]
+            check_1 = base_code.isalpha()
         except KeyError:
-            raise ConvertionException(f"Ошибка!\nУказан неверный код валюты или числовое значение вместо буквенного: {base_code}.\n"
-                                      f"Список поддерживаемых валют доступен по команде '/values' .")
-        try:
-            keys[target_code]
-            target_code.isalpha()
-        except KeyError:
-            raise ConvertionException(f"Ошибка!\nУказан неверный код валюты или числовое значение вместо буквенного: {base_code}.\n"
-                                      f"Список поддерживаемых валют доступен по команде '/values' .")
-
-        try:
-            float(quantity)
-        except ValueError:
             raise ConvertionException(f"Ошибка!\n"
-                                      f"Указанное значение: {quantity} не является числом.")
+                                      f"Указан неверный код валюты или числовое значение вместо буквенного: "
+                                      f"{base_code}.\n"
+                                      f"Список поддерживаемых валют доступен по команде '/values' .")
+        try:
+            check_1 = keys[target_code]
+            check_2 = target_code.isalpha()
+        except KeyError:
+            raise ConvertionException(f"Ошибка!\n"
+                                      f"Указан неверный код валюты или числовое значение вместо буквенного: "
+                                      f"'{base_code}'.\n"
+                                      f"Список поддерживаемых валют доступен по команде '/values' .")
 
         return result['conversion_result']
 
