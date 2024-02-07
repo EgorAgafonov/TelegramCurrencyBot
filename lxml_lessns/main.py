445d12c53c4a98bbbe70d179441a5ab40c70ed92
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-02-07 18:37:13 +0300

    Files updated

--- lxml_lessns/main.py
+++ lxml_lessns/main.py
@@ -46,17 +46,16 @@ import easyocr
 
 def text_recognition(file_path):
     reader = easyocr.Reader(['ru', "en"])
-    result = reader.readtext(file_path, detail=1, output_format='json')
+    result = reader.readtext(file_path, detail=0,  paragraph=True)
     return result
 
 
 def main():
     file_path = input("Enter a file path: ")
     recognized_text = text_recognition(file_path=file_path)
-    # result = ' '.join(recognized_text)
-    print(recognized_text)
+    recognized_string = '\n'.join(recognized_text)
+    print(recognized_string)
 
-# 'dict' 'json' 'free_merge'
 
 if __name__ == "__main__":
     main()
