6fb982645f0be326a9b05fc36d464427ea9df391
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-02-18 12:46:31 +0300

    Files updated

--- utilities.py
+++ utilities.py
@@ -64,7 +64,44 @@ class RequestsToEGRYUL:
     def find_org_by_name(organization_data):
         dadata = Dadata(TOKEN_DADATA)
         response = dadata.suggest('party', organization_data)
-        return response
+        metro = response[0]["data"]["address"]["data"]['metro']
+        tax_system = response[0]['data']['finance']
+        if metro is None:
+            metro_check = "нет"
+            metro_dist = "нет"
+        else:
+            metro_check = metro[0]['name']
+            metro_dist = metro[0]['distance']
+        if tax_system is None:
+            tax_check = "сведения отсутствуют"
+        else:
+            tax_check = tax_system['tax_system']
+
+        full_with_opf = response[0].get('data').get('name').get('full_with_opf')
+        short_with_opf = response[0].get('data').get('name').get('short_with_opf')
+
+
+        result = []
+
+        # result = (f"Полное наимен-ие: <b>{}</b>\n"
+        #           f"Краткое наимен-ие: <b>{}</b>\n"
+        #           f"ИНН: <b>{response[0].get('data').get('inn')}</b>\n"
+        #           f"КПП: <b>{response[0].get('data').get('kpp')}</b>\n"
+        #           f"ОГРН: <b>{response[0].get('data').get('ogrn')}</b>\n"
+        #           f"Дата рег-ии: "
+        #           f"<b>{datetime.datetime.fromtimestamp(((response[0].get('data').get('state').get('registration_date')) / 1000))}</b>\n"
+        #           f"ФИО руков-ля(ЕИО): <b>{response[0].get('data').get('management').get('name')}</b>\n"
+        #           f"Должность руков-ля: <b>{response[0].get('data').get('management').get('post')}</b>\n"
+        #           f"Статус ЮЛ (действ./не действ.): <b>{response[0].get('data').get('state').get('status')}</b>\n"
+        #           f"Код налог-ой инсп-ции: <b>{response[0].get('data').get('address').get('data').get('tax_office')}</b>\n"
+        #           f"Основной ОКВЭД: <b>{response[0].get('data').get('okved')}</b>\n"
+        #           f"Сведения о лицен-ях: <b>{response[0].get('data').get('licenses')}</b>\n"
+        #           f"Система налогооб-ия: <b>{tax_check}</b>\n"
+        #           f"Адрес госуд-ой рег-ии: <b>{response[0].get('data').get('address').get('value')}</b>\n"
+        #           f"Ближайшее метро: <b>м. {metro_check}</b>\n"
+        #           f"Расстояние до метро: <b>{metro_dist}</b>\n")
+
+        return result
 
 
 class TextImageReader:
